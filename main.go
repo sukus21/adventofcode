0d603ac1a48db048f310a8320bb58a42da117d06
sukus <sukus21@gmail.com>
2023-12-08 08:40:49 +0100

    Add ternary helper function
    I am sick and tired of GO not having a ternary operator, so I am making my own

--- main.go
+++ main.go
@@ -515,3 +515,11 @@ func quickconv(str string) int {
 	n, _ := strconv.ParseInt(str, 10, 64)
 	return int(n)
 }
+
+func ternary[T any](condition bool, truthy T, falsy T) T {
+	if condition {
+		return truthy
+	} else {
+		return falsy
+	}
+}
